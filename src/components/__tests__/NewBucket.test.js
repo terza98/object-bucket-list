2f4aabd06ffbdf94997e569eb156c15a6b098d9a
Mladen Terzic <terza98@hotmail.rs>
2020-08-31 12:30:51 +0200

    Added unit tests for NewBucket, rendering and submiting form

--- src/components/__tests__/NewBucket.test.js
+++ src/components/__tests__/NewBucket.test.js
@@ -1,3 +1,40 @@
 import React from 'react';
 import NewBucket from '../NewBucket.js';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
+
+let container = null;
+beforeEach(() => {
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	// cleanup on exiting
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+it('renders without crashing', () => {
+	ReactDOM.render(<NewBucket />, container);
+});
+
+it('matches snapshot', () => {
+	const { asFragment } = render(<NewBucket />);
+
+	expect(asFragment()).toMatchSnapshot();
+});
+
+test('should submit when clicking submit button', () => {
+	const handleSubmit = jest.fn();
+	const { getByTestId } = render(<NewBucket onSubmit={handleSubmit} />);
+
+	const button = getByTestId('button');
+
+	fireEvent.click(button);
+
+	expect(handleSubmit).toHaveBeenCalled();
+});
