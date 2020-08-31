5fa4e2281ba728d1f7f685f1164221298883e1b1
Mladen Terzic <terza98@hotmail.rs>
2020-08-31 12:43:01 +0200

    Added testing for Header and snapshots

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import Header from '../Header';
+import Header from '../Header.js';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
+
+let container = null;
+beforeEach(() => {
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	// cleanup on exiting
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+it('renders without crashing', () => {
+	ReactDOM.render(<Header />, container);
+});
+
+it('matches snapshot', () => {
+	const { asFragment } = render(<Header />);
+
+	expect(asFragment()).toMatchSnapshot();
+});
 
 test('renders simple title', () => {
 	const title = 'Example Title';
