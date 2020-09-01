2ede84459baa6e78be86c28f31255a5bc27bfc94
Mladen Terzic <terza98@hotmail.rs>
2020-09-01 19:37:22 +0200

    changed object test structure

--- src/makeTests.js
+++ src/makeTests.js
@@ -41,11 +41,23 @@ export const locations = {
 };
 
 export const objects = {
-	objects: {
-		name: 'process-line.png',
-		size: 11952,
-		last_modified: '2020-08-29T11:20:04.736247742Z',
-	},
+	objects: [
+		{
+			name: 'IMG_0006 2.jpeg',
+			size: 3046523,
+			last_modified: '2020-08-31T09:45:25.16968728Z',
+		},
+		{
+			name: 'UX-web-page.pdf',
+			size: 1593375,
+			last_modified: '2020-09-01T11:06:11.362814647Z',
+		},
+		{
+			name: 'process-line.png',
+			size: 11952,
+			last_modified: '2020-08-29T11:20:04.736247742Z',
+		},
+	],
 };
 
 export const bucket = {
