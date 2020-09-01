58763afad80c58b48ea5c7c2008ece130c169b1f
Mladen Terzic <terza98@hotmail.rs>
2020-09-01 17:28:25 +0200

    mocking  axios and adding more test routes

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,10 +1,20 @@
-import { locations } from '../makeTests.js';
+import { locations, buckets, objects, bucket } from '../makeTests.js';
 
 export default {
 	get: jest.fn().mockImplementation(url => {
 		switch (url) {
 			case 'https://challenge.3fs.si/storage/locations':
 				return Promise.resolve({ data: locations });
+
+			case 'https://challenge.3fs.si/storage/buckets':
+				return Promise.resolve({ data: buckets });
+
+			case 'https://challenge.3fs.si/storage/buckets/my-awesome-bucket/objects':
+				return Promise.resolve({ data: objects });
+
+			case 'https://challenge.3fs.si/storage/buckets/my-awesome-bucket':
+				return Promise.resolve({ data: bucket });
+
 			default:
 				throw new Error(`UNMATCHED URL: ${url}`);
 		}
