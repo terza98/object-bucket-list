af354f423470351f7db3d577083992e837f7a802
Mladen Terzic <terza98@hotmail.rs>
2020-09-01 17:27:27 +0200

    addded tests for objects  and buckets

--- src/makeTests.js
+++ src/makeTests.js
@@ -1,22 +1,31 @@
-const makeBuckets = n => {
-	// returns n number of buckets items
-	// default is 15
-	const num = n || 15;
-	const buckets = [];
-	for (let i = 0; i < num; i++) {
-		buckets.push({
-			id: `bucket-item-${i}`,
-			name: `Bucket item ${i}`,
+export const buckets = {
+	buckets: [
+		{
+			id: 'my-awesome-bucket',
+			name: 'my-awesome-bucket',
 			location: {
-				id: 'a0c51094-05d9-465f-8745-6cd9ee45b96d',
+				id: '541909F3-20FC-4382-A8E8-18042F5E7677',
 				name: 'Kranj',
 			},
-		});
-	}
-	return buckets;
+		},
+		{
+			id: 'newbucket',
+			name: 'newBucket',
+			location: {
+				id: '571E30F3-7A96-45FF-8FDE-0A3F0E6BBDF4',
+				name: 'Ljubljana',
+			},
+		},
+		{
+			id: 'one-more-bucket',
+			name: 'One more bucket',
+			location: {
+				id: '541909F3-20FC-4382-A8E8-18042F5E7677',
+				name: 'Kranj',
+			},
+		},
+	],
 };
-
-export const buckets = makeBuckets(200);
 
 export const locations = {
 	locations: [
@@ -29,4 +38,23 @@ export const locations = {
 			name: 'Ljubljana',
 		},
 	],
+};
+
+export const objects = {
+	objects: {
+		name: 'process-line.png',
+		size: 11952,
+		last_modified: '2020-08-29T11:20:04.736247742Z',
+	},
+};
+
+export const bucket = {
+	bucket: {
+		id: 'my-awesome-bucket',
+		name: 'my-awesome-bucket',
+		location: {
+			id: '541909F3-20FC-4382-A8E8-18042F5E7677',
+			name: 'Kranj',
+		},
+	},
 };
